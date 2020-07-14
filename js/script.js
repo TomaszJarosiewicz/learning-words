94ca4a9baa0be1a5a588c4faa95387c95b6f6f1b
Tomasz Jarosiewicz <tomasz@jarosiewicz.pl>
2020-07-14 08:50:47 +0200

    Add module for script.js

--- js/script.js
+++ js/script.js
@@ -1,66 +1,4 @@
-const words =
-    [
-        {
-            id: 1,
-            request: 'zabawny',
-            response: 'funny',
-            description: null
-        },
-        {
-            id: 2,
-            request: 'brzuch',
-            response: 'stomach',
-            description: null
-        },
-        {
-            id: 3,
-            request: 'twarz',
-            response: 'face',
-            description: null
-        },
-        {
-            id: 4,
-            request: 'Przepis',
-            response: 'Recipe',
-            description: null
-        },
-        {
-            id: 5,
-            request: 'przychodnia',
-            response: 'clinic',
-            description: null
-        },
-        {
-            id: 6,
-            request: 'samolot',
-            response: 'plane',
-            description: null
-        },
-        {
-            id: 7,
-            request: 'ciężar',
-            response: 'weight',
-            description: null
-        },
-        {
-            id: 8,
-            request: 'nosić',
-            response: 'carry',
-            description: 'Elephants carry weight loads.'
-        },
-        {
-            id: 9,
-            request: 'nosić',
-            response: 'wear',
-            description: 'She wear designer clothes.'
-        },
-        {
-            id: 10,
-            request: 'biurko',
-            response: 'desk',
-            description: null
-        }
-    ];
+import { words } from './words.js';
 
 class LearningWords {
     constructor(words) {
@@ -70,34 +8,28 @@ class LearningWords {
     }
 
     random() {
-        const wordRandom = this.allWords[Math.floor(Math.random() * this.allWords.length)];
-        return wordRandom;
+        return this.allWords[Math.floor(Math.random() * this.allWords.length)];
     }
 
     drawWord() {
         if(this.chooseWords.length === this.allWords.length) {
             this.enableChoose = true;
             return;
+        }
+        if(this.chooseWords.length === 0) {
+            this.chooseWords.push(this.random());
+            return this.chooseWords;
         } else {
-            if(this.chooseWords.length === 0) {
-                this.chooseWords.push(this.random());
-                return this.chooseWords;
-            } else {
-                this.chooseWords.find(idx => {
-                    return idx;
-                });
+            do {
+                this.random();
             } while (this.chooseWords.find(word => this.chooseWords.id === word.id));
         }
-    }
-
-    currentWord() {
-
     }
 }
 
 
 const learningWords = new LearningWords(words);
-learningWords.currentWord();
+console.log(learningWords.drawWord());
 
 
 
