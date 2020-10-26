665c7f004a7fac931dc6b2c3197827d6285fb513
Tomasz Jarosiewicz <tomasz@jarosiewicz.pl>
2020-10-26 20:04:52 +0100

    Progress

--- js/script.js
+++ js/script.js
@@ -1,159 +1,171 @@
 import { words } from './words.js';
 
 class LearningWords {
-    constructor(words) {
-        this.allWords = Object.assign(words);
-        this.chooseWords = [];
-        this.enableChoose = false;
-        this.isButtonActiveVisibility = true;
-        this.currentWord = {};
-        this.newWords = {};
-        this.createWords = {};
+  constructor(words) {
+    this.allWords = Object.assign(words);
+    this.chooseWords = [];
+    this.enableChoose = false;
+    this.isButtonActiveVisibility = true;
+    this.currentWord = {};
+    this.newWord = {};
+  }
+
+  random() {
+    this.currentWord = this.allWords[Math.floor(Math.random() * this.allWords.length)];
+  }
+
+  drawWord() {
+    if(this.chooseWords.length === this.allWords.length) {
+      this.enableChoose = true;
+      return;
     }
 
-    random() {
-        this.currentWord = this.allWords[Math.floor(Math.random() * this.allWords.length)];
+    if(this.chooseWords.length === 0) {
+      this.random();
+      this.chooseWords.push(this.currentWord);
+    } else {
+        do {
+          this.random();
+      } while(this.chooseWords.find(word => this.currentWord.id === word.id));
+      this.chooseWords.push(this.currentWord);
     }
+  }
 
-    drawWord() {
-        if(this.chooseWords.length === this.allWords.length) {
-            this.enableChoose = true;
-            return;
-        }
-        if(this.chooseWords.length === 0) {
-            this.random();
-            this.chooseWords.push(this.currentWord);
-        } else {
-            do {
-                this.random();
-            } while (this.chooseWords.find(word => this.currentWord.id === word.id));
-            this.chooseWords.push(this.currentWord);
-        }
-    }
-    addNewWords() {
-        this.allWords.find(word => {
-            if(this.newWords.request === word.request) {
-                console.log('Error');
-            } else {
-                this.createWords.id = word.id + 1;
-                this.createWords.request = this.newWords.request;
-                this.createWords.response = this.newWords.response;
-                this.createWords.description = this.newWords.description;
-            }
-        });
-        this.allWords.push(this.createWords);
-        console.log(this.allWords.length)
-    }
+  addNewWord(newWord) {
+    this.allWords = [...this.allWords, {...newWord,
+      id: this.allWords.length + 1,
+      request: this.newWord.request,
+      response: this.newWord.response,
+      description: this.newWord.description,
+    }];
+  }
+
+  removeRepeatWord(){
+    this.allWords.find(word => {
+      if(this.newWord.request === word.request) {
+        console.log('error');
+      } else {
+        this.addNewWord();
+        console.log('this.allWords', this.allWords);
+      }
+    });
+  }
 
 }
+
 
 const learningWords = new LearningWords(words);
 learningWords.drawWord();
 
 (() => {
-    const numberQuestion = document.querySelector('#numberQuestion');
-    const lastNumberQuestion = document.querySelector('#lastNumberQuestion');
-    const boxWords = document.querySelector('#box-words');
-    const repeatWords = document.querySelector('#repear-word');
-    const addForm = document.querySelector('#add-form');
-    const btnForm = document.querySelector('#btn-form');
-    let counter = 0;
-    btnForm.style.display = "none";
+  const numberQuestion = document.querySelector('#numberQuestion');
+  const lastNumberQuestion = document.querySelector('#lastNumberQuestion');
+  const boxWords = document.querySelector('#box-words');
+  const repeatWords = document.querySelector('#repeat-word');
+  const addForm = document.querySelector('#add-form');
+  const btnForm = document.querySelector('#btn-form');
+  let counter = 0;
+  btnForm.style.display = "none";
 
-    const enableDisplay = (firstElement, secondElement) => {
-        firstElement.style.display = "block";
-        secondElement.style.display = "block";
-    }
+  const enableDisplay = (firstElement, secondElement) => {
+    firstElement.style.display = 'block';
+    secondElement.style.display = 'block';
+  }
 
-    const disableDisplay = (firstElement, secondElement) => {
-        firstElement.style.display = "none";
-        secondElement.style.display = "none";
-    }
+  const disableDisplay = (firstElement, secondElement) => {
+    firstElement.style.display = 'none';
+    secondElement.style.display = 'none';
+  }
 
-    const activeElement = (enabledElement, disabledElement) => {
-        // enabledElement.disabled = !learningWords.isButtonActiveVisibility;
-        // disabledElement.disabled = learningWords.isButtonActiveVisibility;
-        enabledElement.style.display = 'block';
-        disabledElement.style.display = 'none';
-    }
+  const activeElement = (enabledElement, disabledElement) => {
+    // enabledElement.disabled = !learningWords.isButtonActiveVisibility;
+    // disabledElement.disabled = learningWords.isButtonActiveVisibility;
+    enabledElement.style.display = 'block';
+    disabledElement.style.display = 'none';
+  }
 
-    const displayWords = () => {
-        const { currentWord } = learningWords;
-        boxWords.children[0].innerHTML = currentWord.request;
-        boxWords.children[1].innerHTML = currentWord.response;
-        boxWords.children[2].innerHTML = currentWord.description;
-    }
+  const displayWords = () => {
+    const { currentWord } = learningWords;
+    boxWords.children[0].innerHTML = currentWord.request;
+    boxWords.children[1].innerHTML = currentWord.response;
+    boxWords.children[2].innerHTML = currentWord.description;
+  }
 
+  disableDisplay(boxWords.children[1], boxWords.children[2]);
+  disableDisplay(repeatWords, addForm);
+
+  const showBtn = document.querySelector('#showBtn');
+  const nextQuestion = document.querySelector('#next');
+  const repeatBtn = document.querySelector('#repeat');
+  const addWords = document.querySelector('.add-word');
+  repeatBtn.style.display = 'none';
+
+  const { allWords, chooseWords } = learningWords;
+
+  lastNumberQuestion.innerHTML = allWords.length;
+  numberQuestion.innerHTML = chooseWords.length;
+
+  activeElement(showBtn, nextQuestion);
+
+  showBtn.addEventListener('click', () => {
+    enableDisplay(boxWords.children[1], boxWords.children[2]);
+    displayWords();
+    activeElement(nextQuestion, showBtn);
+  });
+
+  nextQuestion.addEventListener('click', () => {
+    counter++;
     disableDisplay(boxWords.children[1], boxWords.children[2]);
-    disableDisplay(repeatWords, addForm);
-
-    const showBtn = document.querySelector('#showBtn');
-    const nextQuestion = document.querySelector('#next');
-    const repeatBtn = document.querySelector('#repeat');
-    const addWords = document.querySelector('.add-word');
-    repeatBtn.style.display = 'none';
-
-    const { allWords, chooseWords } = learningWords;
-    lastNumberQuestion.innerHTML = allWords.length;
+    learningWords.drawWord();
+    displayWords();
     numberQuestion.innerHTML = chooseWords.length;
+    // if(chooseWords.length === allWords.length) {
+    //   nextQuestion.disabled = learningWords.isButtonActiveVisibility;
+    // }
+    if(counter < allWords.length) {
+      activeElement(showBtn, nextQuestion);
+    } else {
+      disableDisplay(nextQuestion, showBtn);
+      repeatBtn.style.display = 'block';
+      activeElement(repeatWords, boxWords);
+    }
+});
 
-    activeElement(showBtn, nextQuestion);
+  displayWords(btnForm.children[0], btnForm.children[1]);
 
-    showBtn.addEventListener('click', () => {
-        enableDisplay(boxWords.children[1], boxWords.children[2]);
-        displayWords();
-        activeElement(nextQuestion, showBtn);
-    });
+  repeatBtn.addEventListener('click', () => {
+    location.reload();
+  });
 
-    nextQuestion.addEventListener('click', () => {
-        counter++;
-        disableDisplay(boxWords.children[1], boxWords.children[2]);
-        learningWords.drawWord();
-        displayWords();
-        numberQuestion.innerHTML = chooseWords.length;
-        // if(chooseWords.length === allWords.length) {
-        //     nextQuestion.disabled = learningWords.isButtonActiveVisibility;
-        // }
-        if(counter < allWords.length) {
-            activeElement(showBtn, nextQuestion);
-        } else {
-            disableDisplay(nextQuestion, showBtn);
-            repeatBtn.style.display = 'block';
-            activeElement(repeatWords, boxWords);
-        }
-    });
+  addWords.addEventListener('click', () => {
+    activeElement(btnForm, showBtn);
+    addForm.style.display = "block";
+  });
 
-    displayWords(btnForm.children[0], btnForm.children[1]);
+  btnForm.children[0].addEventListener('click', () => {
+    disableDisplay(btnForm, addForm);
+    showBtn.style.display = "block";
+  });
 
-    repeatBtn.addEventListener('click', () => {
-        location.reload();
-    });
+  const inputs = document.querySelectorAll('#add-form > input');
 
-    addWords.addEventListener('click', () => {
-        activeElement(btnForm, showBtn);
-        addForm.style.display = "block";
-    });
+  btnForm.children[1].addEventListener('click', () => {
+    for(let input of inputs) {
+      if(input.value === '') {
+        input.style.border = "1px solid #b14506";
+        // learningWords.addNewWords = input.children[0];
+      } else {
+        input.style.border = "1px solid #706f6f";
+      }
+    }
 
-    btnForm.children[0].addEventListener('click', () => {
-        disableDisplay(btnForm, addForm);
-        showBtn.style.display = "block";
-    });
+    learningWords.newWord = {
+      request: inputs[0].value,
+      response: inputs[1].value,
+      description: inputs[2].value
+    }
 
-    const inputs = document.querySelectorAll('#add-form > input');
-
-    btnForm.children[1].addEventListener('click', () => {
-        for(let input of inputs) {
-            if(input.value === '') {
-                input.style.border = "1px solid #b14506";
-                // learningWords.addNewWords = input.children[0];
-            } else {
-                input.style.border = "1px solid #706f6f";
-            }
-        }
-        learningWords.newWords.request = inputs[0].value;
-        learningWords.newWords.response = inputs[1].value;
-        learningWords.newWords.description = inputs[2].value;
-        learningWords.addNewWords();
-    });
+    learningWords.removeRepeatWord();
+  });
 
 })();
--- js/words.js
+++ js/words.js
@@ -1,63 +1,63 @@
 export const words =
-    [
-        {
-            id: 1,
-            request: 'zabawny',
-            response: 'funny',
-            description: null
-        },
-        {
-            id: 2,
-            request: 'brzuch',
-            response: 'stomach',
-            description: null
-        },
-        {
-            id: 3,
-            request: 'twarz',
-            response: 'face',
-            description: null
-        },
-        {
-            id: 4,
-            request: 'przepis',
-            response: 'recipe',
-            description: null
-        },
-        {
-            id: 5,
-            request: 'przychodnia',
-            response: 'clinic',
-            description: null
-        },
-        {
-            id: 6,
-            request: 'samolot',
-            response: 'plane',
-            description: null
-        },
-        {
-            id: 7,
-            request: 'ciężar',
-            response: 'weight',
-            description: null
-        },
-        {
-            id: 8,
-            request: 'nosić',
-            response: 'carry',
-            description: 'Elephants carry weight loads.'
-        },
-        {
-            id: 9,
-            request: 'nosić',
-            response: 'wear',
-            description: 'She wear designer clothes.'
-        },
-        {
-            id: 10,
-            request: 'biurko',
-            response: 'desk',
-            description: null
-        }
-    ];
+  [
+    {
+      id: 1,
+      request: 'zabawny',
+      response: 'funny',
+      description: null
+    },
+    {
+      id: 2,
+      request: 'brzuch',
+      response: 'stomach',
+      description: null
+    },
+    {
+      id: 3,
+      request: 'twarz',
+      response: 'face',
+      description: null
+    },
+    {
+      id: 4,
+      request: 'przepis',
+      response: 'recipe',
+      description: null
+    },
+    {
+      id: 5,
+      request: 'przychodnia',
+      response: 'clinic',
+      description: null
+    },
+    {
+      id: 6,
+      request: 'samolot',
+      response: 'plane',
+      description: null
+    },
+    {
+      id: 7,
+      request: 'ciężar',
+      response: 'weight',
+      description: null
+    },
+    {
+      id: 8,
+      request: 'nosić',
+      response: 'carry',
+      description: 'Elephants carry weight loads.'
+    },
+    {
+      id: 9,
+      request: 'nosić',
+      response: 'wear',
+      description: 'She wear designer clothes.'
+    },
+    {
+      id: 10,
+      request: 'biurko',
+      response: 'desk',
+      description: null
+    }
+  ];
